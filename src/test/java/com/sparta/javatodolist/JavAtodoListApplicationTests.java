61f0e5f6fa4b786cda733a7500c11d028c43e1d8
dyorcat <yool0417@gmail.com>
2024-07-24 16:44:16 +0900

    JAVA TodoList 프로젝트 생성

---
+++ src/test/java/com/sparta/javatodolist/JavAtodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.javatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAtodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
