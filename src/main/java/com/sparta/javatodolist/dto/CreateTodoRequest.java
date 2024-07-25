834d49b13d5422d8aa24921c663c86b570305c42
dyorcat <yool0417@gmail.com>
2024-07-25 14:54:22 +0900

    CreateTodoRequest 작성

---
+++ src/main/java/com/sparta/javatodolist/dto/CreateTodoRequest.java
@@ -0,0 +1,33 @@
+package com.sparta.javatodolist.dto;
+
+import com.sparta.javatodolist.entity.Todo;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CreateTodoRequest {
+    private String title;
+    private String content;
+    private String assignee;
+    private String password;
+
+
+    public CreateTodoRequest(String title, String content, String assignee, String password) {
+        this.title = title;
+        this.content = content;
+        this.assignee = assignee;
+        this.password = password;
+    }
+
+    public Todo toEntity() {
+        Todo todo = new Todo();
+        todo.setTitle(this.title);
+        todo.setContent(this.content);
+        todo.setAssignee(this.assignee);
+        todo.setPassword(this.password);
+        todo.setCreatedAt(LocalDateTime.now()); // 작성일 설정
+        return todo;
+    }
+
+}
