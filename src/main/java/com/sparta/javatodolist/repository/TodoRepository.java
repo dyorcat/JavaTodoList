7aae60107165ffa2c14662d8b42a4dd1e8820c22
dyorcat <yool0417@gmail.com>
2024-07-25 18:11:55 +0900

    Swagger 설정

--- src/main/java/com/sparta/javatodolist/repository/TodoRepository.java
+++ src/main/java/com/sparta/javatodolist/repository/TodoRepository.java
@@ -4,7 +4,6 @@ import com.sparta.javatodolist.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
