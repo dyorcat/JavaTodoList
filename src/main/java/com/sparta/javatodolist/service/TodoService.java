79a33689b7f0213ec2ebf604a6914b1457f38560
dyorcat <yool0417@gmail.com>
2024-07-25 14:54:42 +0900

    TodoService 작성

---
+++ src/main/java/com/sparta/javatodolist/service/TodoService.java
@@ -0,0 +1,21 @@
+package com.sparta.javatodolist.service;
+
+import com.sparta.javatodolist.dto.CreateTodoRequest;
+import com.sparta.javatodolist.dto.TodoResponse;
+import com.sparta.javatodolist.entity.Todo;
+import com.sparta.javatodolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TodoService {
+    private final TodoRepository todoRepository;
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {this.todoRepository = todoRepository; }
+
+    public TodoResponse createTodo(CreateTodoRequest createTodoRequest) {
+        Todo todo = createTodoRequest.toEntity();
+        Todo savedTodo = todoRepository.save(todo);
+        return new TodoResponse(savedTodo);
+    }
+}
