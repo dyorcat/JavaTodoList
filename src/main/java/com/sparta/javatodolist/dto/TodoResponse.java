46f92b126ca6f6214754f2bcea47504bfaf1c715
dyorcat <yool0417@gmail.com>
2024-07-25 14:54:12 +0900

    TodoResponse 작성

---
+++ src/main/java/com/sparta/javatodolist/dto/TodoResponse.java
@@ -0,0 +1,25 @@
+package com.sparta.javatodolist.dto;
+
+import com.sparta.javatodolist.entity.Todo;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class TodoResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String asignee;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public TodoResponse(Todo todo) {
+        this.id = todo.getId();
+        this.title = todo.getTitle();
+        this.content = todo.getContent();
+        this.asignee = todo.getAssignee();
+        this.createdAt = todo.getCreatedAt();
+        this.modifiedAt = todo.getModifiedAt();
+    }
+}
