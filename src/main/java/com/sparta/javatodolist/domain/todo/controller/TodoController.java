fdb3543d6f0399e7f211bd57aa9f709a32754969
dyorcat <yool0417@gmail.com>
2024-07-27 19:07:39 +0900

    ResponseEntity를 반환하도록 변경

--- src/main/java/com/sparta/javatodolist/domain/todo/controller/TodoController.java
+++ src/main/java/com/sparta/javatodolist/domain/todo/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.sparta.javatodolist.domain.todo.controller;
 
 import com.sparta.javatodolist.domain.todo.dto.CreateTodoRequest;
+import com.sparta.javatodolist.domain.todo.dto.SimplifiedTodoResponse;
 import com.sparta.javatodolist.domain.todo.dto.TodoResponse;
 import com.sparta.javatodolist.domain.todo.dto.UpdateTodoRequest;
 import com.sparta.javatodolist.domain.todo.service.TodoService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,36 +15,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
-
     private final TodoService todoService;
-
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
 
     @PostMapping()
-    public TodoResponse createTodo(@Valid @RequestBody CreateTodoRequest createTodoRequest) {
-        return todoService.createTodo(createTodoRequest);
+    public ResponseEntity<TodoResponse> createTodo(@Valid @RequestBody CreateTodoRequest createTodoRequest) {
+        TodoResponse response = todoService.createTodo(createTodoRequest);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoResponse> getTodo(@PathVariable Long id) {
+        TodoResponse response = todoService.getTodoById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping()
-    public List<TodoResponse> getTodoList() {
-        return todoService.getTodoList();
-    }
-    @GetMapping("/{id}")
-    public TodoResponse getTodo(@PathVariable Long id) {
-        return todoService.getTodo(id);
+    public ResponseEntity<List<SimplifiedTodoResponse>> getTodoList() {
+        List<SimplifiedTodoResponse> response = todoService.getTodoList();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
-    public TodoResponse updateTodo(@PathVariable Long id, @RequestBody UpdateTodoRequest updateTodoRequest) {
-        return todoService.updateTodo(id, updateTodoRequest);
+    @PatchMapping("/{id}")
+    public ResponseEntity<TodoResponse> updateTodo(
+            @PathVariable Long id,
+            @RequestBody UpdateTodoRequest updateTodoRequest) {
+        TodoResponse response = todoService.updateTodo(id, updateTodoRequest);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTodo(@PathVariable Long id, @RequestParam String password) {
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id, @RequestParam String password) {
         todoService.deleteTodo(id, password);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
 }
