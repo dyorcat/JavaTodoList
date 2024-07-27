d22df3968175b939ac5698a2d8b20892ab8f13ef
dyorcat <yool0417@gmail.com>
2024-07-27 19:11:01 +0900

    ToEntity 삭제. 서비스에서 직접 entity로 변경하도록 변경

--- src/main/java/com/sparta/javatodolist/domain/todo/service/TodoService.java
+++ src/main/java/com/sparta/javatodolist/domain/todo/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.sparta.javatodolist.domain.todo.service;
 
 import com.sparta.javatodolist.domain.todo.dto.CreateTodoRequest;
+import com.sparta.javatodolist.domain.todo.dto.SimplifiedTodoResponse;
 import com.sparta.javatodolist.domain.todo.dto.TodoResponse;
 import com.sparta.javatodolist.domain.todo.dto.UpdateTodoRequest;
 import com.sparta.javatodolist.domain.todo.entity.Todo;
@@ -25,19 +26,26 @@ public class TodoService {
     }
 
     public TodoResponse createTodo(CreateTodoRequest createTodoRequest) {
-        Todo todo = createTodoRequest.toEntity();
+        Todo todo = new Todo();
+        todo.setTitle(createTodoRequest.getTitle());
+        todo.setContent(createTodoRequest.getContent());
+        todo.setAssignee(createTodoRequest.getAssignee());
+        todo.setPassword(createTodoRequest.getPassword());
         Todo savedTodo = todoRepository.save(todo);
-        return new TodoResponse(savedTodo);
+        return TodoResponse.toResponse(savedTodo);
     }
 
-    public List<TodoResponse> getTodoList() {
-        return todoRepository.findAllByOrderByCreatedAtDesc().stream().map(TodoResponse::new).toList();
-    }
-
-    public TodoResponse getTodo(Long id) {
+    public TodoResponse getTodoById(Long id) {
         Todo foundTodo = todoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당하는 할 일이 없습니다. "));
-        return new TodoResponse(foundTodo);
+        return TodoResponse.toResponse(foundTodo);
     }
+
+    public List<SimplifiedTodoResponse> getTodoList() {
+        return todoRepository.findAllByOrderByCreatedAtDesc().stream()
+                .map(SimplifiedTodoResponse::fromEntity)
+                .toList();
+    }
+
 
     public TodoResponse updateTodo(Long id, UpdateTodoRequest updateTodoRequest) {
         Todo existingTodo = todoRepository.findById(id)
@@ -49,14 +57,13 @@ public class TodoService {
                     id, updateTodoRequest.getPassword(), existingTodo.getPassword());
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
         }
-
         existingTodo.setTitle(updateTodoRequest.getTitle());
         existingTodo.setContent(updateTodoRequest.getContent());
         existingTodo.setAssignee(updateTodoRequest.getAssignee());
 
         // 수정된 엔티티 저장
         Todo updatedTodo = todoRepository.save(existingTodo);
-        return new TodoResponse(updatedTodo);
+        return TodoResponse.toResponse(updatedTodo);
     }
 
     public void deleteTodo(Long id, String password) {
